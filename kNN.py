a891c6ff70d43d998acda66377fe62cc7c7c7569
LiuXPeng <703762445@qq.com>
2017-10-11 21:27:01 +0800

    2.1

---
+++ kNN.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'knn 2017.10.11'
+
+__author__ = 'liuxiaopeng'
+
+from numpy import *
+import operator
+
+def createDataSet():
+	group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+	labels = ['A', 'A', 'B', 'B']
+	return group, labels
