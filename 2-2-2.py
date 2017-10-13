9c0b48639c293e2e1cffb2c40c770498a42c1116
LiuXPeng <703762445@qq.com>
2017-10-13 21:04:29 +0800

    2.2 is over

--- 2-2-2.py
+++ 2-2-2.py
@@ -7,6 +7,6 @@ import matplotlib.pyplot as plt
 datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
 fig = plt.figure()
 ax = fig.add_subplot(111)
-#ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 15, 16)
-ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 15.0*array(datingLabels), 15.0*array(datingLabels))
+ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 20, 15.0*(array(datingLabels)+20))
+#ax.scatter(datingDataMat[:,0], datingDataMat[:,1], 15.0*array(datingLabels), 15.0*array(datingLabels))
 plt.show()
--- kNN.py
+++ kNN.py
@@ -42,3 +42,37 @@ def file2matrix(filename):
 		index += 1
 	return returnMat, classLabelVector
 
+def autoNorm(dataSet):
+	minVals = dataSet.min(0)
+	maxVals = dataSet.max(0)
+	ranges = maxVals - minVals
+	normDataSet = zeros(shape(dataSet))
+	m = dataSet.shape[0]
+	normDataSet = dataSet - tile(minVals, (m, 1))
+	normDataSet = normDataSet / tile(ranges, (m,1))
+	return normDataSet, ranges, minVals
+
+def datingClassTest():
+	hoRatio = 0.10
+	datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m * hoRatio)
+	errorCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i,:], normMat[numTestVecs:m,:], datingLabels[numTestVecs:m], 3)
+		print('the classifier came back with: %d, the real answer is: %d' % (classifierResult, datingLabels[i]))
+		if classifierResult != datingLabels[i] :
+			errorCount += 1.0
+	print ('the total error rate is: %f' % (errorCount / float(numTestVecs)))
+
+def classifyPerson():
+	resultList = ['not at all', 'in small doses', 'in large doses']
+	percentTats = float(input("percentage of time spent playing video games?"))
+	ffMiles = float(input("frequent flier miles earned per year?"))
+	iceCream = float(input("liters of ice cream consumed per year?"))
+	datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	inArr = array([ffMiles, percentTats, iceCream])
+	classifierResult = classify0((inArr - minVals) / ranges, normMat, datingLabels, 3)
+	print('You will probably like this person: ',resultList[classifierResult - 1])
