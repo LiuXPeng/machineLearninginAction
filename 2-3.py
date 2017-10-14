7fb5af7dead4f2cd22ff109d4b519581a75a57a3
LiuXPeng <703762445@qq.com>
2017-10-14 20:16:13 +0800

    chapt 2 is finished

---
+++ 2-3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'mechineLearninginAction 2-3 program'
+
+__author__ = 'lxp'
+
+import kNN
+kNN.handwritingClassTest()
--- kNN.py
+++ kNN.py
@@ -7,6 +7,7 @@ __author__ = 'liuxiaopeng'
 
 from numpy import *
 import operator
+from os import listdir
 
 def createDataSet():
 	group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
@@ -76,3 +77,42 @@ def classifyPerson():
 	inArr = array([ffMiles, percentTats, iceCream])
 	classifierResult = classify0((inArr - minVals) / ranges, normMat, datingLabels, 3)
 	print('You will probably like this person: ',resultList[classifierResult - 1])
+
+#手写识别系统
+
+def img2vector(filename):
+	returnVect = zeros((1,1024))
+	fr = open(filename)
+	for i in range(32):
+		lineStr = fr.readline()
+		for j in range(32):
+			returnVect[0, 32 * i + j] = int(lineStr[j])
+	return returnVect
+
+def handwritingClassTest():
+	hwLabels = []
+	trainingFileList = listdir('trainingDigits')
+	m = len(trainingFileList)
+	trainingMat = zeros((m, 1024))
+	for i in range(m):
+		fileNameStr = trainingFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		hwLabels.append(classNumStr)
+		trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+	testFileList = listdir('testDigits')
+	errorCount = 0.0
+	mTest = len(testFileList)
+	for i in range(mTest):
+		fileNameStr = testFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+		classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+		print('the classifier came back with: %d, the real answer is: %d' % (classifierResult, classNumStr))
+		if (classifierResult != classNumStr): 
+			errorCount += 1.0
+	print('\nthe total number of errors is: %d' % errorCount)
+	print('\nthe total error rate is: %f' % (errorCount/float(mTest)))
+
+
