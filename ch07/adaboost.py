e9f75876d824f00f97cbc8b199c52ddb6940c9d4
LiuXPeng <703762445@qq.com>
2017-12-12 11:11:02 +0800

    adaboost

---
+++ ch07/adaboost.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'adaboost'
+
+from numpy import *
+
+__author__ = 'lxp'
+
+def loadSimpData():
+	datMat = matrix([[1. , 2.1],
+			[2. , 1.1],
+			[1.3, 1. ],
+			[1. , 1. ],
+			[2. , 1. ]])
+	classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
+	return datMat, classLabelo
+
+def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
+	retArray = ones((shape(dataMatrix)[0], 1))
+	if threshIneq == 'lt':
+		retArray [dataMatrix[:, dimen] <= threshVal] = -1.0
+	else:
+		retArray [dataMatrix[:, dimen] > threshVal] = -1.0
+	return retArray
+
+def buildStump(dataArr, classLabels, D):
+	dataMatrix = mat(dataArr)
+	labelMat = mat(classLabels).T
+	m, n = shape(dataMatrix)
+	numSteps = 10.0
+	bestStump = {}
+	bestClasEst = mat(zeros((m, 1)))
+	minError = inf
+	for i in range(n):
+		rangeMin = dataMatrix[:, i].min()
+		rangeMax = dataMatrix[:, i].max()
+		stepSize = (rangeMax - rangeMin) / numSteps
+		for j in range(-1, int(numSteps) + 1):
+			for inequal in ['lt', 'gt']:
+				threshVal = (rangeMin + float(j) * stepSize)
+				predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
+				errArr = mat(ones((m, 1)))
+				errArr[predictedVals == labelMat] = 0
+				weightedError = D.T * errArr
+				#print("split: dim %d thresh %.2f, thresh ineqal: %s, the weighted error is %.3f" % (i, threshVal, inequal, weightedError))
+				if weightedError < minError:
+					minError = weightedError
+					bestClasEst = predictedVals.copy()
+					bestStump['dim'] = i
+					bestStump['thresh'] = threshVal
+					bestStump['ineq'] = inequal
+	return bestStump, minError, bestClasEst
+	
+def adaBoostTrainDS(dataArr, classLabels, numIt = 40):
+	weakClassArr = []
+	m = shape(dataArr)[0]
+	D = mat(ones((m, 1)) / m)
+	aggClassEst = mat(zeros((m, 1)))
+	for i in range(numIt):
+		bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+		print ("D:", D.T)
+		alpha = float(0.5 * log((1.0 - error) / max(error, 1e-16)))
+		bestStump['alpha'] = alpha
+		weakClassArr.append(bestStump)
+		print ("aggClassEst: ", classEst.T)
+		expon = mulitply(D, exp(expon))
+		D = D / D.sum()
+		aggClassEst += alpha * classEst
+		print ("aggClassEst: ", aggClassEst.T)
+	   	aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T, ones((m, 1)))
+ 		errorRate = aggErrors.sum() / m
+		print ("total error: ",errorRate, '\n')
+		if errorRate == 0.0:
+			break
+	return weakClassArr	
