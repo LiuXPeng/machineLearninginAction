09071c74c86fbf37d7d9220d33fc53199e11216a
LiuXPeng <whyLiuXPeng@users.noreply.github.com>
2018-01-02 20:05:30 +0800

    11.5

---
+++ ch11/11_4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'11.3 machine learning in action'
+
+__author__ = 'lxp'
+
+import apriori
+
+dataSet = apriori.loadDataSet()
+L, supportData = apriori.apriori(dataSet, 0.5)
+rules = apriori.generateRules(L, supportData, 0.7)
+print (rules)
