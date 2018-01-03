7918ff18ca040479f4e6e60322af8f110fb45955
LiuXPeng <whyLiuXPeng@users.noreply.github.com>
2018-01-03 19:50:28 +0800

    12

--- ch12/fpGrowth.py
+++ ch12/fpGrowth.py
@@ -20,3 +20,46 @@ class treeNode():
 		print (' ' * ind, self.name, ' ', self.count)
 		for child in self.children.values():
 			child.disp(ind + 1)
+
+def createTree(dataSet, minSup = 1):
+	headerTable = {}
+	for tans in dataSet:
+		for  item in trans:
+			headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
+	for k in list(headerTable):
+		if headerTable[k] < minSup:
+			del(headerTable)
+	freqItemSet = set(headerTable.keys())
+	if len(freqItemSet) == 0:
+		return None, None
+	for k in headerTable:
+		headerTable[k] = [headerTable[k], None]
+	retTree = treeNode('Null Set', 1, None)
+	for tranSet, count in dataSet.items():
+		localD = {}
+		for item in tranSet:
+			if item in freqItemSet:
+				localD[item] = headerTable[item][0]
+		if len(localD) > 0:
+			orderdItems = [v[0] for v in sorted(localD.items(), key = lambda p: p[1], reverse = True)]
+			updateTree(orderdItems, retTree, headerTable, count)
+	return retTree, headerTable
+
+def updateTree(items, inTree, headerTable, count):
+	if items[0] in inTree.children:
+		inTree.children[items[0]], inc(count)
+	else:
+		inTree.children[items[0]] = treeNode(items[0], count, inTree)
+		if headerTable[items[0]][1] ==None:
+			headerTable[items[0]][1] = inTree.children[items[0]]
+		else:
+			updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
+	if len(items) > 1:
+		updateTree(items[1::], inTree.children[items[0]], headerTable, count)
+
+
+def udateHeader(nodeToTest, targetNode):
+	while (nodeToTest.nodeLink != None):
+		nodeToTest = nodeToTest.nodeLink
+	nodeToTest.nodeLink = targetNode
+
