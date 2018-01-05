9d4744d47fc99b4529523762149952763ace4038
LiuXPeng <whyLiuXPeng@users.noreply.github.com>
2018-01-05 17:02:30 +0800

    ·12

--- ch12/fpGrowth.py
+++ ch12/fpGrowth.py
@@ -77,3 +77,29 @@ def createInitSet(dataSet):
 	for trans in dataSet:
 		retDict[frozenset(trans)] = 1
 	return retDict
+
+def ascendTree(leafNode, prefixPath):
+	if leafNode.parent != None:
+		prefixPath.append(leafNode.name)
+		ascendTree(leafNode.parent, prefixPath)
+
+def findPrefixPath(basePat, treeNode):
+	condPats = {}
+	while treeNode != None:
+		prefixPath = []
+		ascendTree(treeNode, prefixPath)
+		if len(prefixPath) > 1:
+			condPats[frozenset(prefixPath[1:])] = treeNode.count
+		treeNode = treeNode.nodeLink
+	return condPats
+
+def mineTree(inTree, headerTable, minSup, preFix, freqItemList):
+	bigL = [v[0] for v in sorted(headerTable.items(), key = lambda p: p[1])]	
+	for basePat in bigL:
+		newFreqSet = preFix.copy()
+		newFreqSet.add(basePat)
+		freqItemList.append(newFreqSet)
+		condPattBases = findPrefixPath(basePat, headerTable[basePat][1])
+		myCondTree, myHead = createTree(condPattBases, minSup)
+		if myHead != None:
+			mineTree(myCondTree, myHead, minSup, newFreqSet, freqItemList)
